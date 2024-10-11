1edf0b440dacbc04cdda089c16728c48a3519901
marilyn.musyimi <marilyn.musyimi@student.moringaschool.com>
2024-10-11 15:42:14 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    // Find the Superbowl winning record for the Denver Broncos
+    const win = record.find(game => game.result === 'W');
+    
+    // Return the year as a string if a winning record is found; otherwise return undefined
+    return win ? win.year.toString(2015) : undefined;
+  }
+
